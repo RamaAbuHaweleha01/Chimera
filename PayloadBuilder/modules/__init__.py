d6e0eea4c5791018fd9cfa7f2c050643d459b514
RamaAbuHaweleha01 <ramaa6673@gmail.com>
2026-08-03 03:58:47 +0300

    Decryption done

--- PayloadBuilder/modules/__init__.py
+++
@@ -1,7 +0,0 @@
-# Chimera Malware - Modules
-from .system_info import SystemInfo
-from .keylogger import Keylogger
-from .screenshot import Screenshot
-from .password_stealer import PasswordStealer
-from .browser_data import BrowserData
-from .c2_client import C2Client
